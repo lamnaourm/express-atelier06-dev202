aedd90873d59bf7ff1185b0c446fd31ac2ec9357
lamnaourm <mohammed.lamnaour@gmail.com>
2023-01-03 11:59:39 +0100

    les routes de actor

--- routes/actor.js
+++ routes/actor.js
@@ -1,17 +1,34 @@
-const express = require('express')
-const ActorsModel = require('../models/ActorModel')
+const express = require("express");
+const ActorsModel = require("../models/ActorModel");
+const MovieModel = require("../models/MovieModel");
 
-const routes = express.Router(); 
+const routes = express.Router();
 
-routes.get('/all',  (req, res) => {
-
-   ActorsModel.find({}, (err, actor) => {
-        if(!err)
-            res.json(actor);
-        else 
-            res.status(510).json({message: err});
-   });
-
+routes.get("/all", (req, res) => {
+  ActorsModel.find({}, (err, actor) => {
+    if (!err) res.json(actor);
+    else res.status(510).json({ message: err });
+  });
 });
 
-module.exports = routes
+routes.get("/names", (req, res) => {
+  ActorsModel.distinct("name", (err, actor) => {
+    if (!err) res.json(actor);
+    else res.status(510).json({ message: err });
+  });
+});
+
+routes.get("/movies", (req, res) => {
+  const step1 = { $unwind: "$actors" };
+  const step2 = { $group: { _id: "$actors", nb_film: { $sum: 1 } } };
+  const step3 = {
+    $project: { _id: 0, acteur: "$_id", nombre_film: "$nb_film" },
+  };
+
+  MovieModel.aggregate([step1, step2, step3], (err, actors) => {
+    if (!err) res.json(actors);
+    else res.status(510).json({ message: err });
+  });
+});
+
+module.exports = routes;
